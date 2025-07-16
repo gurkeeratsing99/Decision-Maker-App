ae69e13a2f0e9d22b64987f12490f4953de59215
jasdeepsidhu0312 <jasdeepsidhu0312@gmail.com>
2025-07-15 21:39:41 -0700

    Update App.jsx

--- DecisionMaker/src/App.jsx
+++ DecisionMaker/src/App.jsx
@@ -21,6 +21,8 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/search" element={<Search />} />
         <Route path="/surprise" element={<Surprise />} />
+        <Route path="/history" element={<History />} />
+
 
       </Routes>
     </Router>
